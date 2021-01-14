0d1e2ff59d3095e776475bdc75fdcb88e43718be
davidtc8 <davidtorresc8@gmail.com>
2021-01-13 20:05:26 -0600

    Guess the state game!

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,7 +1,9 @@
 import pandas
 import turtle
+import time
 
 screen = turtle.Screen()
+screen.setup(width=700, height=550)
 screen.title('🗺️ U.S. States Game 🗺️')
 
 # load in a new image as a new shape
@@ -11,6 +13,7 @@ turtle.shape(image)
 
 # Boolean to keep the game running
 game_is_on = True
+style = ('Courier', 10, 'bold')
 
 # Number of guesses correct
 correct_guesses = 0
@@ -26,7 +29,7 @@ while game_is_on:
     message = f'Correct States {correct_guesses}/50'
 
     # check the answer
-    answer_state = screen.textinput (title=message, prompt='Guess a State')
+    answer_state = screen.textinput (title=message, prompt='Guess a State').title()
     guess = answer_state
     print(guess)
 
@@ -35,10 +38,26 @@ while game_is_on:
         print('Element Exists')
         correct_guesses += 1
         correct_states.append(guess)
+        word = turtle.Turtle()
+        word.hideturtle()
+        word.penup()
+        position = df[df.state == guess]
+        position_x = int(position.x)
+        position_y = int(position.y)
+        print(position_x, position_y)
+        word.goto(position_x, position_y)
+        word.write(guess, font=style, align='center')
         print('elements inside the list: ', correct_states)
         print(correct_guesses)
     else:
-        print('lokura')
+        word_2 = turtle.Turtle()
+        word_2.hideturtle()
+        word_2.color('red')
+        word_2.penup()
+        word_2.goto(x=250, y=250)
+        word_2.write(f'{guess} is not a state', font=style, align='center')
+        time.sleep(2)
+        word_2.clear()
 
 # get the coordinates in the map
 # this can help you create another map from another country if you like
